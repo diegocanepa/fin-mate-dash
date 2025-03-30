efbed53849edae89140f08b798d8e8b95f434e6b
diegocanepa <diego.canepa241198@gmail.com>
2025-03-30 20:53:29 -0300

    change supabase key env variable

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 // Determinar el esquema a utilizar basado en la variable de entorno
 // Si no está definida, usar 'public' por defecto (producción)
