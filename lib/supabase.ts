e9e0b0520ce287a6c126d68ac5e5239397d281cb
diegocanepa <diego.canepa241198@gmail.com>
2025-03-30 21:07:29 -0300

    Add log to test functionality

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,11 +6,15 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 // Determinar el esquema a utilizar basado en la variable de entorno
 // Si no está definida, usar 'public' por defecto (producción)
 export const DB_SCHEMA = process.env.NEXT_PUBLIC_DB_SCHEMA || "public"
+console.log("Supabase DB_SCHEMA:", DB_SCHEMA);
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
 // Función auxiliar para obtener una referencia a una tabla con el esquema configurado
 // Alternativa si la primera solución no funciona
 export function getTableWithSchema(tableName: string) {
-  return supabase.schema(DB_SCHEMA).from(tableName);
+  var table
+  table = supabase.schema(DB_SCHEMA).from(tableName);
+  console.log("Supabase table_name:", table);
+  return tableName
 }
